91c1bc008cabd296d85c89cb7a4ae1dd1822a030
jd <msirojuddin26@gmail.com>
2023-09-14 14:29:28 +0700

    tes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,8 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        if(is_LoggedIn()) return view('utamas');
-        $error = session()->getFlashdata('error');        
+        if(is_LoggedIn()) return view('pages/utamas');
+        $error = session()->getFlashdata('pages/error');        
         $data = empty($error)?[]:$error; 
         return view('login',$data);
     }
